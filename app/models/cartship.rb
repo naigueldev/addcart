7c3e825daa0aa43e6e510e7b7c80b2ef28635b98
Naiguel Santos <naiguel@criativitta.com.br>
2017-03-15 14:59:55 -0300

    edit git commands

--- app/models/cartship.rb
+++ app/models/cartship.rb
@@ -6,9 +6,7 @@ class Cartship < ApplicationRecord
   def add_item
     # current_item = cart.cartships.find_by_product_id(product_id)
     # current_item = cart.cartships.where('product_id = ?', product_id).first
-
     self.quantity = self.quantity + 1
-
   end
 
 end
